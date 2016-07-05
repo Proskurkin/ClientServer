f4e2bed163f37a6f9801f2113ac24fa832e2cc52
Евгений <eugene.proskurkin@gmail.com>
2016-07-05 08:08:11 +0300

    firstServlet

---
+++ mavenproject1-ejb/src/main/java/servlets/RegistrationServlet.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Евгений on 05.07.2016.
+ */
+@WebServlet("/registration")
+public class RegistrationServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("registration");
+    }
+}

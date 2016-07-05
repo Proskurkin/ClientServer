13fb3cc3cfffccb9a9cae23c7f74fbf59b33641a
Евгений <eugene.proskurkin@gmail.com>
2016-07-05 07:48:45 +0300

    openRegistr

---
+++ mavenproject1-ejb/src/main/java/servlets/SignInServlet.java
@@ -0,0 +1,21 @@
+package servlets;
+
+import sun.net.httpserver.HttpServerImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Евгений on 05.07.2016.
+ */
+@WebServlet("/signIn")
+public class SignInServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.toString());
+    }
+}

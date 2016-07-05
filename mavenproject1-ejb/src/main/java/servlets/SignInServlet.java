3c5708b8738f156d1ca2bfdc4c0b79aff04db759
eproskurin <eproskurin@unit-org.ru>
2016-07-05 14:24:41 +0300

    1

--- mavenproject1-ejb/src/main/java/servlets/SignInServlet.java
+++ mavenproject1-ejb/src/main/java/servlets/SignInServlet.java
@@ -8,14 +8,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by Евгений on 05.07.2016.
  */
-@WebServlet("/signIn")
+@WebServlet(value = "/signIn", name = "signIn")
 public class SignInServlet extends HttpServlet {
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("+++++++++++++++++++++++++++++++");
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello Servlet Get</h1>");
+        out.println("</body>");
+        out.println("</html>");
     }
 }
---
+++ mavenproject1-web/src/main/webapp/login.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: eproskurin
+  Date: 05.07.2016
+  Time: 12:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form action="/signIn/" method="POST">
+    <input type="submit" value="Submit form "/>
+    </form>
+</body>
+</html>
